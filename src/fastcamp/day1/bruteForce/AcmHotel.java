c3b571d15476d4a32fcf8cdce6d28600b18838b1
LIMDOYOUNG\dlaeh <lian.dy220@gmail.com>
2023-09-18 08:00:52 +0900

    오늘의집+전체탐색

---
+++ src/fastcamp/day1/bruteForce/AcmHotel.java
@@ -0,0 +1,36 @@
+package fastcamp.day1.bruteForce;
+
+import java.util.Scanner;
+
+public class AcmHotel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int[][] rooms = new int[T][3];
+        for (int i = 0; i < T; i++) {
+            rooms[i] = new int[]{sc.nextInt(), sc.nextInt(), sc.nextInt()};
+        }
+
+        for (int i = 0; i < T; i++) {
+            getRoomNumber(rooms[i][0], rooms[i][1], rooms[i][2]);
+        }
+    }
+
+    private static void getRoomNumber(int H, int W, int num) {
+        int floor = ((num - 1) % H) + 1;
+        int roomNumber = (num - 1) / H + 1;
+        System.out.printf("%d%02d\n", floor, roomNumber);
+    }
+
+//    private static void getRoomNumber(int H, int W, int num) {
+//        int floor = num % H;
+//        int roomNumber = num / H + 1;
+//        if (floor == 0) {
+//            floor = H;
+//            roomNumber -= 1;
+//        }
+//        System.out.println(floor + "0" + roomNumber);
+//    }
+
+
+}
